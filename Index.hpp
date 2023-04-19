bba76df8a71b91a22ada818829bfb85119dc12b7
Lp700750 <2357543706@qq.com>
2023-04-19 20:48:50 +0800

    index

--- Index.hpp
+++ Index.hpp
@@ -25,6 +25,7 @@ namespace index
   class Index
   {
   public:
+    //建立正排索引
     Docfo* farwardSearch(uint64_t doc_id)
     {
       if(doc_id>=farwardIndex.size())
@@ -33,6 +34,8 @@ namespace index
       }
       return &farwardIndex[doc_id];
     }
+
+    //建立倒排索引
     std::vector<Node>* invertSearch(const std::string& word)
     {
       auto iter=invertIndex.find(word);
@@ -42,6 +45,125 @@ namespace index
         return nullptr;
       }
       return &(iter->second);
+    }
+
+    //根据解析得到的文件，构建由倒排索引到正排索引的过程
+    bool builtIndex(const std::string& input)
+    {
+      std::ifstream in(input,std::ios::in|std::ios::binary);
+      if(!in.is_open())
+      {
+        std::cout<<"input open fail!"<<std::endl;
+        return false;
+      }
+      std::string line;
+      while(std::getline(in,line))
+      {
+        //进行正排索引
+        Docfo* doc=builtFarwardIndex(line);
+        if(doc==nullptr)
+        {
+          std::cout<<"builtFarwardIndex"<<std::endl;
+          continue;
+        }
+
+        //进行倒排索引
+        builtInvertIndex(*doc);
+
+      }
+      return true;
+    }
+  private:
+    Docfo* builtFarwardIndex(const std::string& line)
+    {
+      //存放每一个html文档的标题，内容，url的数组
+      std::vector<std::string> result;
+      
+      //设置3部分的分隔符
+      const std::string sep="\3";
+
+      Lp700::stringTool::split(line,&result,sep);
+
+      //如果分离出来的结果不是三部分，就说明有问题
+      if(3!=result.size())
+      {
+        std::cout<<"split fail"<<std::endl;
+        return  nullptr;
+      }
+
+      //将分离好的html三部分装入数组当中
+      Docfo doc;
+      doc.title=result[0];
+      doc.content=result[1];
+      doc.url=result[2];
+
+      //紧跟着数组的下标作为其文档的id
+      doc.docId=farwardIndex.size();
+
+      //push_back()会进行拷贝，导致效率会降低
+      farwardIndex.push_back(std::move(doc));
+      
+      return &farwardIndex.back();
+    }
+    
+    //
+    bool builtInvertIndex(const Docfo& doc)
+    {
+      //统计某一个词的标题和内容的大小
+      struct word_cnt
+      {
+        int title_cnt;
+        int content_cnt;
+        word_cnt()
+          :title_cnt(0)
+          ,content_cnt(0)
+        {}
+      };
+      
+      //建立一个某一个词与其词频统计的映射表
+      std::unordered_map<std::string,word_cnt> wordMap;
+
+      //用来装标题当中分词的数组
+      std::vector<std::string> titleVec;
+
+      //对标题进行jieba分词
+      Lp700::jiebaTool::cutString(doc.title,&titleVec);
+
+      //对标题当中的分词进行词频统计
+      for(std::string s:titleVec)
+      {
+        //将所有的单词全都转换成小写
+        boost::to_lower(s);
+        
+        //如果wordMap[s]存在的话就查找，如果没有的话就会新建
+        wordMap[s].title_cnt++;
+      }
+
+      //用来转内容当中分词的数组
+      std::vector<std::string> contentVec;
+
+      //用来对内进行分词处理
+      Lp700::jiebaTool::cutString(doc.content,&contentVec);
+
+      //对内容当中的分词进行词频统计
+      for(std::string s:contentVec)
+      {
+        boost::to_lower(s);
+        wordMap[s].content_cnt++;
+      }
+      
+      //进行相关度分析
+      for(auto &word_pair:wordMap)
+      {
+        Node node;
+        node.docid=doc.docId;
+        node.keyword=word_pair.first;
+        node.weight=10*word_pair.second.title_cnt+word_pair.second.content_cnt;
+        std::vector<Node>& invertList=invertIndex[word_pair.first];
+
+        invertList.push_back(std::move(node));
+      }
+      return true;
     }
   private:
     std::vector<Docfo> farwardIndex;
