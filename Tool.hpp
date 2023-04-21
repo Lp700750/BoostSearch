d85bda11ff7eefa29fd6572f08502745795ce92c
Lp700750 <2357543706@qq.com>
2023-04-21 21:05:34 +0800

    tool

--- Tool.hpp
+++ Tool.hpp
@@ -4,6 +4,7 @@
 #include<vector>
 #include<fstream>
 #include<boost/algorithm/string.hpp>
+#include"jieba/Jieba.hpp"
 namespace Lp700
 {
   class fileTool
@@ -34,4 +35,34 @@ namespace Lp700
       return true;
     }
   };
+
+  //对每一个html文档标题 内容 url进行分离
+  class stringTool
+  {
+  public:
+    static bool Split(const std::string &target,std::vector<std::string> *out,const std::string& gep)
+    {
+      //使用boost自带的分割工具
+      boost::split(*out,target,boost::is_any_of(gep),boost::token_compress_on);
+    }
+  };
+
+  const char* const DICT_PATH = "./dict/jieba.dict.utf8";
+  const char* const HMM_PATH = "./dict/hmm_model.utf8";
+  const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
+  const char* const IDF_PATH = "./dict/idf.utf8";
+  const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
+   
+
+  class jiebaUtil
+  {
+  private:
+    static cppjieba::Jieba jieba;
+  public:
+    static void CutString(const std::string& src,std::vector<std::string>* out)
+    {
+      jieba.CutForSearch(src,*out);
+    }
+  };
+  cppjieba::Jieba jiebaUtil::jieba(DICT_PATH,HMM_PATH,USER_DICT_PATH,IDF_PATH,STOP_WORD_PATH);
 }
